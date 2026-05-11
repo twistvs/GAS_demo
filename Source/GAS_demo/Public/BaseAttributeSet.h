86b3177ced5c30776f3e7a5d191b7c1f010afa74
twistvs <twistmahua@gmail.com>
2026-05-11 22:40:07 +0800

    把AttributeSet导入了

--- Source/GAS_demo/Public/BaseAttributeSet.h
+++ Source/GAS_demo/Public/BaseAttributeSet.h
@@ -4,11 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseAttributeSet.generated.h"
+
 
 /**
  * 
+ * 
  */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class GAS_DEMO_API UBaseAttributeSet : public UAttributeSet
 {
@@ -18,16 +27,26 @@ public:
 	//可编辑，被蓝图可读可写，被分在BaseAttributeSet类别下
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData HP;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, HP)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData MaxHP;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxHP)
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData MP;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MP)
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData MaxMP;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxMP)
+	
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData Strength;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Strength)
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttributeSet")
 	FGameplayAttributeData MaxStrength;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxStrength)
+	
 };
